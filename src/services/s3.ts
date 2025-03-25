05f85a57f48433e9d828548837169a34130f25ce
Babyze <danghuynhanh1999@gmail.com>
2025-03-25 15:37:42 +0700

    Update check object exist

--- src/services/s3.ts
+++ src/services/s3.ts
@@ -1,5 +1,9 @@
 import { S3_BUCKET } from "../constants/s3-bucket.constant";
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+  GetObjectCommand,
+  HeadObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export default class S3Service {
@@ -12,6 +16,19 @@ export default class S3Service {
       Bucket: bucket,
       Key: key,
     });
-    return getSignedUrl(this.s3Client, getObjectCommand);
+    const isObjectExist = await this.isObjectExist(bucket, key);
+    if (isObjectExist) {
+      return getSignedUrl(this.s3Client, getObjectCommand);
+    }
+    return "";
+  }
+
+  async isObjectExist(bucket: string, key: string) {
+    const headObjectCommand = new HeadObjectCommand({
+      Bucket: bucket,
+      Key: key,
+    });
+    const result = await this.s3Client.send(headObjectCommand);
+    return !!result;
   }
 }
