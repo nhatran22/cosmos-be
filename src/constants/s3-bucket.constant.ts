e22be5e9c4f363e42b7051b0ad1a2e4f72578505
Babyze <danghuynhanh1999@gmail.com>
2025-03-25 15:50:06 +0700

    Remove Region redundant

--- src/constants/s3-bucket.constant.ts
+++ src/constants/s3-bucket.constant.ts
@@ -1,6 +1,5 @@
 import { defaultTo } from "lodash";
 
 export const S3_BUCKET = {
-  BUCKET_REGION: defaultTo(process.env.BUCKET_REGION, ""),
   IMAGE_BUCKET: defaultTo(process.env.COSMOS_SYSTEM_IMAGES_BUCKET_NAME, ""),
 };
--- src/services/s3.ts
+++ src/services/s3.ts
@@ -1,11 +1,8 @@
-import { S3_BUCKET } from "../constants/s3-bucket.constant";
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export default class S3Service {
-  private s3Client = new S3Client({
-    region: S3_BUCKET.BUCKET_REGION,
-  });
+  private s3Client = new S3Client();
 
   async getDownloadSignedUrl(bucket: string, key: string) {
     const getObjectCommand = new GetObjectCommand({
